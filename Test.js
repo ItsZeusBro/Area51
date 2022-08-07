1ee562934e65e1ddca806acb7b106b9e96560625
Zeus <zeusofolympus1000@gmail.com>
2022-08-07 00:28:10 -0400

    annoying typechecking before assertion means we need to run the function twice!

--- Test.js
+++ Test.js
@@ -1,4 +1,5 @@
 import { Recursion } from "./Recursion.js";
+import * as assert from "node:assert"
 
 class Test{
     constructor(){
@@ -10,79 +11,119 @@ class Test{
     tests(){
         this._orderedPinchSet()
         this._orderedExpandSet()
-        this._experiments()
+        this.inputOutput()
 
     }
+
     inputOutput(){
+        Array.prototype.equals = function(arr2) {
+            return (
+              this.length === arr2.length &&
+              this.every((value, index) => value === arr2[index])
+            );
+          };
         var recursion = new Recursion()
         for(var i=0; i<10; i++){
-            assert.deepEquals(
-                recursion.rotate(this.setArr,  i),
-                recursion.rotate(this.setStr, i)
+            assert.equal(
+                recursion.rotate(this.setArr,  i).equals(recursion.rotate(this.setStr, i)),
+                true  
             )
             if(i){
-                assert.deepEquals(
-                    recursion.rotate(this.setArr, -i),
-                    recursion.rotate(this.setStr, -i)
+                assert.equal(
+                    recursion.rotate(this.setArr,  -i).equals(recursion.rotate(this.setStr, -i)),
+                    true  
                 )
             }
 
 
             for(var j=0; j<10; j++){
                 
-                assert.deepEquals(
-                    recursion.swap(this.setArr,  i, j),
-                    recursion.swap(this.setStr, i, j)
+                assert.equal(
+                        recursion.swap(this.setArr,  i, j)
+                    .equals(
+                        recursion.swap(this.setStr, i, j)
+                    ),
+                    true    
                 )
 
-                assert.deepEquals(
-                    recursion.iterJSlice(this.setArr,  i, j),
-                    recursion.iterJSlice(this.setStr, i, j)
+                assert.equal(
+                        recursion.iterJSlice(this.setArr,  i, j)
+                    .equals(
+                        recursion.iterJSlice(this.setStr, i, j)
+                    ),
+                    true   
                 )
 
-                assert.deepEquals(
-                    recursion.reverseIterJSlice(this.setArr,  i, j),
-                    recursion.reverseIterJSlice(this.setStr, i, j)
+                assert.equal(
+                        recursion.reverseIterJSlice(this.setArr,  i, j)
+                    .equals(
+                        recursion.reverseIterJSlice(this.setStr, i, j)
+                    ),
+                    true  
                 )
 
                 for(var r=0; r<10; r++){
                     
 
-                    assert.deepEquals(
-                        recursion.iterIJSliceRotate(this.setArr,  i, j, r),
-                        recursion.iterIJSliceRotate(this.setStr, i, j, r)
+                    assert.equal(
+                            recursion.iterIJSliceRotate(this.setArr,  i, j, r)
+                        .equals(
+                            recursion.iterIJSliceRotate(this.setStr, i, j, r)
+                        ),
+                        true
                     )
 
                     for(var n=0; n<10; n++){
 
-                        assert.deepEquals(
-                            recursion.iterJSliceRotate(this.setArr,  i, j, r, n),
-                            recursion.iterJSliceRotate(this.setStr, i, j, r, n)
+                        assert.equal(
+                                recursion.iterJSliceRotate(this.setArr,  i, j, r, n)
+                            .equals(
+                                recursion.iterJSliceRotate(this.setStr, i, j, r, n)
+                            ),
+                            true
+                            
                         )
 
-                        assert.deepEquals(
-                            recursion.iterJSliceRotate(this.setArr,  i, j, r, n),
-                            recursion.iterJSliceRotate(this.setStr, i, j, r, n)
+                        assert.equal(
+                                recursion.iterJSliceRotate(this.setArr,  i, j, r, n)
+                            .equals(
+                                recursion.iterJSliceRotate(this.setStr, i, j, r, n)
+                            ),
+                            true
                         )
 
                         for(var s=0; s<10; s++){
-                            assert.deepEquals(
-                                recursion.iterJSliceRotateSwap(this.setArr,  i, j, s, r, n),
-                                recursion.iterJSliceRotateSwap(this.setStr, i, j, s, r, n)
-                            )
-                            assert.deepEquals(
-                                recursion.iterIJSliceRotateSwap(this.setArr,  i, j, s, r, n),
-                                recursion.iterIJSliceRotateSwap(this.setStr, i, j, s, r, n)
+
+                            assert.equal(
+                                    recursion.iterJSliceRotateSwap(this.setArr,  i, j, s, r, n)
+                                .equals(
+                                    recursion.iterJSliceRotateSwap(this.setStr, i, j, s, r, n)
+                                ),
+                                true
                             )
 
-                            assert.deepEquals(
-                                recursion.iterIJSSliceRotateSwap(this.setArr,  i, j, s, r, n),
-                                recursion.iterIJSSliceRotateSwap(this.setStr, i, j, s, r, n)
+                            assert.equal(
+                                    recursion.iterIJSliceRotateSwap(this.setArr,  i, j, s, r, n)
+                                .equals(
+                                    recursion.iterIJSliceRotateSwap(this.setStr, i, j, s, r, n)
+                                ),
+                                true
                             )
 
-                            assert.deepEquals(
-                                recursion.reverseIterIJSliceRotate(this.setArr,  i, j, r, n),
-                                recursion.reverseIterIJSliceRotate(this.setStr, i, j, r, n)
+                            assert.equal(
+                                    recursion.iterIJSSliceRotateSwap(this.setArr,  i, j, s, r, n)
+                                .equals(
+                                    recursion.iterIJSSliceRotateSwap(this.setStr, i, j, s, r, n)
+                                ),
+                                true
+                            )
+
+                            assert.equal(
+                                    recursion.reverseIterIJSliceRotate(this.setArr,  i, j, r, n)
+                                .equals(
+                                    recursion.reverseIterIJSliceRotate(this.setStr, i, j, r, n)
+                                ),
+                                true
                             )
                         }
                     }
