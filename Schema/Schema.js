b39309fe45c8dc940c4563cfc544f6b9cd28be05
zeus <zeusofolympus1000@gmail.com>
2022-08-13 03:17:41 -0400

    somethingis recursive if the key is not in payload keys and if it is associated with a value that is an object or array

--- Schema/Schema.js
+++ Schema/Schema.js
@@ -6,36 +6,36 @@ export class Schema{
     paths(tree, path, _paths=[], pk=['payload'], rk=[]){
 		path=path.slice()
 		if(Array.isArray(tree)){
-			//if its an object it is passed down a level
-			//if its a raw type it is payload, and we add a payload step with general 'payload' key associated with a payload array
 			var payload=[]
 			for(var i = 0; i<tree.length; i++){
-				if(Array.isArray(tree[i])){
-					//if its an array, pass it down
-					this.paths(tree[i], path, _paths, pk, rk)
-				}else if(typeof tree[i]==='object'){
-					//if its an object, pass it down
+				if(Array.isArray(tree[i]) || typeof tree[i]==='object'){
+					//if its an array or object, pass it down
 					this.paths(tree[i], path, _paths, pk, rk)
 				}else{
+					//its a raw value, push to payload
 					payload.push(tree[i])
 				}
 			}
-			//use build step add to path, add to _paths, and return
-			return _paths.push(path.push(this.build_step('payload', payload)))
-
+			//use build step add payload to path, add to _paths, and return
+			if(payload.length){
+				return _paths.push(path.push(this.build_step('payload', payload)))
+			}
+			return _paths
 		}else if(typeof tree ==='object'){
-			//if its a recursive key, add it to rk
 			for(var i = 0; i<Object.keys(tree); i++){
 				var key = Object.keys(tree)[i]
-				if(this.is_recursive(tree, key)){
-					path.push(this.build_step(key, this.get_payload(tree, pk)))
-					if(!rk.includes(key)){
-						rk.push(key)
-					}
-					this.paths(tree[key], path, _paths, pk, rk)
-				}else if(this.is_base(tree, key)){
 
-				}else if(this.is_payload(tree, key)){
+				if(this.is_recursive(tree, key, pk)){
+					//add recursive key if not included
+					if(!rk.includes(key)){rk.push(key)}
+					//add recursive step to path
+					path.push(this.build_step(key, this.get_payload(tree, pk)))
+					//recursively call this.paths
+					this.paths(tree[key], path, _paths, pk, rk)
+				}else if(rk.includes(key)){
+					//its a base case or its payload, add to path, then to paths
+					return _paths.push(path.push(this.build_step(key, tree[key])))
+				}else{
 
 				}
 			}
@@ -63,12 +63,8 @@ export class Schema{
 		}
 		return payload
 	}
-	is_recursive(obj, pk){
-
-	}
-	is_base(obj, pk){
-		//if object has a key not in pk and rk then its a base case key
-		if()
+	is_recursive(obj, key, pk){
+		//something is recursive if the key is not in pk and if it is associated with a value that is an object or array
 	}
     
 }
