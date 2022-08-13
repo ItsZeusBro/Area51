d2d12865c8e456772290669a4a054d96ee5d92f1
Zeus <zeusofolympus1000@gmail.com>
2022-08-13 05:49:42 -0400

    it looks good

--- Schema/Schema.js
+++ Schema/Schema.js
@@ -3,39 +3,42 @@ export class Schema{
 
     }
 	//payload is key-wise or value-based payload, so if its recursable, its identified by payload key, if its not, its identified as raw values
-    paths(tree, pk=['payload'], path=[], _paths=[], rk=[]){
+    paths(tree, pk=['payload'], path=[], _paths=[]){
+		path=path.slice()
+
 		if(Array.isArray(tree)){
 			//collect payload (if it exists), add it to path with a general payload key
-			var [payload, schema]=this.separate(tree, pk, rk)
-			console.log(payload, schema)
+			var [payload, schema]=this.separate(tree, pk)
 			//check if base case
 			if(schema.length){
 				//it recursive
-			}else{
-				//its base case
+				for(var i=0; i<schema.length; i++){
+					path.push({[i]:payload})
+					this.paths(schema[i], pk, path, _paths)
+				}
 			}
 
 		}else if(typeof tree === 'object'){
 			//collect all payload in pk's (if it exists), add it to path with a general payload key
-			var [payload, schema]=this.separate(tree, pk, rk)
-			console.log(payload, schema)
+			var [payload, schema]=this.separate(tree, pk)
 			//base case here is defined if the object does not contain recursable values associated with non-payload keys
 			if(Object.keys(schema).length){
-				//its recursive
-			}else{
-				//its a base case
+				for(var i=0; i<Object.keys(schema).length; i++){
+					var key=Object.keys(schema)[i]
+					path.push({[key]:payload})
+					this.paths(schema[key], pk, path, _paths)				
+				}
 			}
 
 		}else{
-			throw Error("schema invalid")
+			path.push({[tree]:payload})
+			_paths.push(path)
+			return _paths
 		}
+		return _paths
 
     }    
 	
-	build_step(key, payload){
-		return {[key]:payload}
-	}
-
 	separate(obj, pk){
 		//separate payload from recursable values
 		if(Array.isArray(obj)){
@@ -66,13 +69,6 @@ export class Schema{
 			return [payload, schema]
 		}
 	}
-	is_recursive(obj, key, pk){
-		//something is recursive if the key is not in pk and if it is associated with a value that is an object or array
-		if(!pk.includes(key)&&(Array.isArray(obj[key]||typeof obj[key] === 'object'))){
-			return true
-		}
-	}
-    
 }
 
 const SCHEMA=[
@@ -192,4 +188,4 @@ const SCHEMA=[
 ]
   
 var schema = new Schema()
-schema.paths(SCHEMA, [], [], ['~DEFAULT~'], [])
+console.log(schema.paths(SCHEMA, ['~DEFAULT~']))
