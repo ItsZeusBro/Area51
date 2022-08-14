5b3b0495ef5b8dc5b83025f205b671095bd4471a
Zeus <zeusofolympus1000@gmail.com>
2022-08-14 01:29:12 -0400

    redoing this AGAIN!

--- Schema/Schema.js
+++ Schema/Schema.js
@@ -1,80 +1,19 @@
 import * as util from "node:util"
 
 export class Schema{
-    constructor(){
-
-    }
-	//payload is key-wise or value-based payload, so if its recursable, its identified by payload key, if its not, its identified as raw values
-    paths(tree, pk=['payload'], path=[], _paths=[]){
-		path=path.slice()
-
-		if(Array.isArray(tree)){
-			//collect payload (if it exists), add it to path with a general payload key
-			var [payload, schema]=this.separate(tree, pk)
-			//check if base case
-			if(schema.length){
-				//it recursive
-				for(var i=0; i<schema.length; i++){
-					path.push({[i]:payload})
-					this.paths(schema[i], pk, path, _paths)
-				}
-			}
-
-		}else if(typeof tree === 'object'){
-			//collect all payload in pk's (if it exists), add it to path with a general payload key
-			var [payload, schema]=this.separate(tree, pk)
-			//base case here is defined if the object does not contain recursable values associated with non-payload keys
-			if(Object.keys(schema).length){
-				for(var i=0; i<Object.keys(schema).length; i++){
-					var key=Object.keys(schema)[i]
-					path.push({[key]:payload})
-					this.paths(schema[key], pk, path, _paths)				
-				}
-			}
-
-		}else{
-			var [payload, schema]=this.separate(tree, pk)
-			path.push({[schema]:payload})
-			_paths.push(path)
-			return _paths
-		}
-		return _paths
-
-    }    
-	
-	separate(obj, pk){
-		//separate payload from recursable values
-		if(Array.isArray(obj)){
-			var payload=[];
-			var schema = [];
-			for(var i =0; i<obj.length; i++){
-				//if its an array or object value then its apart of schema
-				//else its a part of payload
-				if(Array.isArray(obj[i]) || typeof obj[i]==='object'){
-					schema.push(obj[i])
-				}else{
-					payload.push(obj[i])
-				}
-			}
-			return [payload, schema]
-
-		}else if(typeof obj === 'object'){
-			var payload = {};
-			var schema = {};
-			for(var i=0; i<Object.keys(obj).length; i++){
-				var key=Object.keys(obj)[i]
-				if(pk.includes(key)){
-					payload[key] = obj[key]
-				}else{
-					schema[key] = obj[key]
-				}
-			}
-			return [payload, schema]
-		}else{
-			//there is no difference between payload and schema, so its a base case
-			return [undefined, obj]
-		}
+	paths(){
+		//payload is either defined by a key if its an object, or it is defined as something that is not an array
+		//or not an object (inside of an array)
+		//base case 1 raw value
+		//recursive case 1 array
+			//if its an array, there is payload, but no key so each payload has its own step at the index
+			//{indexKey:payload}
+		//recursive case 2 object 
+			//{key:payload at level of the key}
+		
 	}
+
+	
 	log(obj){
         if(obj){
             console.log(util.inspect(obj, false, null, true))
@@ -85,114 +24,119 @@ export class Schema{
 const SCHEMA=[
 	{
 		'isEncArr': [
-		{
-		'isEnc': [
-		{
-			'isArr': [
-			{ '~DEFAULT~': 'utf8', 'isEnc': 'wackyFunction1' }
-			]
-		}
-		]
-		},
-		{
-		'isStrArr': [
-		{
-			'~DEFAULT~': [ 'utf8' ],
-			'isEncArr': [
 			{
-			'~DEFAULT~': 'Wm',
-			'isStr': 'wackyFunction2'
+				'isEnc': [
+					{
+						'isArr': [
+							{ 
+								'~DEFAULT~': 'utf8', 
+								'isEnc': 'wackyFunction1' 
+							}
+						]
+					}
+				]
 			},
 			{
-			'~DEFAULT~': [ { '6K': { 'hDz': undefined } }, undefined ],
-			'isObjArr': 'wackyFunction3'
-			}
-			]
-		},
-		{
-			'isStrArr': [
-			{
-			'~DEFAULT~': [
-			{ 'hLM': undefined },
-			{ 'HMi': { 'FpT': undefined } },
-			undefined
-			],
-			'isObjArr': 'wackyFunction4'
+				'isStrArr': [
+					{
+						'~DEFAULT~': [ 'utf8' ],
+						'isEncArr': [
+							{
+								'~DEFAULT~': 'Wm',
+								'isStr': 'wackyFunction2'
+							},
+							{
+								'~DEFAULT~': [ { '6K': { 'hDz': undefined } }, undefined ],
+								'isObjArr': 'wackyFunction3'
+							}
+						]
+					},
+					{
+						'isStrArr': [
+							{
+								'~DEFAULT~': [
+									{ 'hLM': undefined },
+									{ 'HMi': { 'FpT': undefined } },
+									undefined
+								],
+								'isObjArr': 'wackyFunction4'
+							},
+							{
+								'~DEFAULT~': [ 'utf8' ],
+								'isEncArr': 'wackyFunction5'
+							}
+						]
+					}
+				]
 			},
 			{
-			'~DEFAULT~': [ 'utf8' ],
-			'isEncArr': 'wackyFunction5'
+				'~DEFAULT~': { 'G': { S5: { 'i': undefined } } },
+				'isObj': [
+					{
+						'isEnc': [ { 'isArr': 'wackyFunction6' } ]
+					}
+				]
 			}
-			]
-		}
-		]
-		},
-		{
-		'~DEFAULT~': { 'G': { S5: { 'i': undefined } } },
-		'isObj': [
-		{
-			'isEnc': [ { 'isArr': 'wackyFunction6' } ]
-		}
-		]
-		}
 		]
 	},
 	{
 		'isStr': [
-		{
-		'~DEFAULT~': [],
-		'isIntArr': [
-		{
-			'isArr': [
 			{
-			'~DEFAULT~': [ 0, 2, 3 ],
-			'isIntArr': 'wackyFunction7'
+				'~DEFAULT~': [],
+				'isIntArr': [
+					{
+						'isArr': [
+							{
+								'~DEFAULT~': [ 0, 2, 3 ],
+								'isIntArr': 'wackyFunction7'
+							}
+						]
+					}
+				]
 			}
-			]
-		}
-		]
-		}
 		]
 	},
 	{
 		'~DEFAULT~': [],
 		'isArr': [
-		{
-		'~DEFAULT~': [
-		{ 'c': undefined },
-		{ 'iDI': { 'o': { 'y': undefined } } }
-		],
-		'isObjArr': [
-		{
-			'~DEFAULT~': [ 2 ],
-			'isIntArr': [
 			{
-			'isStrArr': 'wackyFunction8'
+				'~DEFAULT~': [
+					{ 'c': undefined },
+					{ 'iDI': { 'o': { 'y': undefined } } }
+				],
+				'isObjArr': [
+					{
+						'~DEFAULT~': [ 2 ],
+						'isIntArr': [
+							{
+							'isStrArr': 'wackyFunction8'
+							},
+							{
+							'~DEFAULT~': { 'p0': undefined },
+							'isObj': 'wackyFunction9'
+							}
+						]
+					},
+					{
+						'isIntArr': [ 
+							{ '~DEFAULT~': 2, 'isInt': 'wackyFunction10' } 
+						]
+					}
+				]
 			},
 			{
-			'~DEFAULT~': { 'p0': undefined },
-			'isObj': 'wackyFunction9'
+				'isStr': [
+					{
+						'~DEFAULT~': '',
+						'isStr': [
+							{
+								'~DEFAULT~': [ 'Uvo', 'evH' ],
+								'isStrArr': 'wackyFunction11'
+							}
+						]
+					}
+				]
 			}
-			]
-		},
-		{
-			'isIntArr': [ { '~DEFAULT~': 2, 'isInt': 'wackyFunction10' } ]
-		}
-		]
-		},
-		{
-		'isStr': [
-		{
-			'~DEFAULT~': '',
-			'isStr': [
-			{
-			'~DEFAULT~': [ 'Uvo', 'evH' ],
-			'isStrArr': 'wackyFunction11'
-			}
-			]
-		}
-		]
-		}
 		]
 	},
   1
@@ -200,25 +144,25 @@ const SCHEMA=[
 
 var SCHEMA2={
 	'one':[
-		1, 
-		2, 
-		3, 
-		{
-			'oneNHalf':'sdlfkjdsflkj'
-		},
-		[
-			'dlfkjsdf',
-			2,
+			1, 
+			2, 
 			3, 
-			4, 
-			5
-		],
-		{
-			'~DEFAULT~':{}
-		},
-		{
-			'payload':{}
-		}
+			{
+				'oneNHalf':'sdlfkjdsflkj'
+			},
+			[
+				'dlfkjsdf',
+				2,
+				3, 
+				4, 
+				5
+			],
+			{
+				'~DEFAULT~':{}
+			},
+			{
+				'payload':{}
+			}
 	],
 
 	'two':{
